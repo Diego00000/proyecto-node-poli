e72a9af24af6a385d41eeb6f16ba30bf0eb25c14
Brandon Rodriguez <ba.rodriguez1995@gmail.com>
2025-10-07 17:19:13 -0400

    Implementacion de ruta clientes.

--- server.js
+++ server.js
@@ -13,6 +13,16 @@ app.get('/', (req, res) => {
     res.json({ mensaje: 'Servicio SOA con Node.js funcionando correctamente' });
 });
 
+// Endpoint GET /clientes
+app.get('/clientes', (req, res) => {
+  const clientes = [
+    { id: 1, nombre: 'Juan Pérez', correo: 'juan.perez@empresa.com', ciudad: 'Bogotá' },
+    { id: 2, nombre: 'María Gómez', correo: 'maria.gomez@empresa.com', ciudad: 'Medellín' },
+    { id: 3, nombre: 'Carlos López', correo: 'carlos.lopez@empresa.com', ciudad: 'Cali' },
+  ];
+  res.json(clientes);
+});
+
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
